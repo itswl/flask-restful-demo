7c165b05845b9cf4847351201925a59994570d59
WeiLai <imwl@live.com>
2020-01-08 10:56:07 +0800

    Update __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,1 +1,13 @@
+from flask_restful import Api
+from flask import Blueprint
+from app.api.v1.test import Hello
 
+
+def register_views(app):
+    api = Api(app)
+    api.add_resource(Hello, '/hello', endpoint="hello")
+
+def  create_blueprint_v1():
+    bp_v1 = Blueprint('v1', __name__)
+    register_views(bp_v1)
+    return bp_v1
